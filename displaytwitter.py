ade0c3c137ebc633a1a23c9d4eb66a702f37540e
Kevin Conner <kevin.r.conner@gmail.com>
2014-05-12 13:30:58 -0700

    further work on separating long strings into four lines

--- displaytwitter.py
+++ displaytwitter.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python
 #Python 3.4
+
+##Documentation
+#twitter - https://dev.twitter.com/docs
+#twython - http://twython.readthedocs.org/en/latest/
 from twython import Twython, TwythonError
 
 #to keep API keys out of version control create apikeys.py. 
 #it onlyneeds to contain the two definitions below
-import apikeys
+import apikeys, time, sys, os, serial
 APP_KEY = apikeys.APP_KEY
 APP_SECRET = apikeys.APP_SECRET
 
-# Requires Authentication as of Twitter API v1.1
+#requires Authentication as of Twitter API v1.1
 twitter = Twython(APP_KEY, APP_SECRET)
 
 #define what strings you are searching Twitter for and how many of each result you wish to see.
-tags = ['#trtt2014','tinkerfest','RogueHackLab','#RHL','#ScienceWorks']
-results_per_tag = 5
-
+tags = ['#trtt2014']#,'tinkerfest','RogueHackLab','#ScienceWorks', '@Soupala']
+results_per_tag = 1
+	
 def GetTweets(String, Count):
 	try:
 		search_results = twitter.search(q=String, count=Count)
@@ -22,13 +26,50 @@ def GetTweets(String, Count):
 		print (e)
 	return search_results
 
-tweets = {}
-for tag in tags:
-	search_results = GetTweets(tag, results_per_tag)
-	for tweet in search_results['statuses']:
-		if tweet['id'] not in tweets:
-			tweets[tweet['id']] = tweet
-			#msg.append("@%s-> %s" % (tweet['user']['screen_name'].encode('utf-8'), tweet['text'].encode('utf-8')))
+def TweetDict(tags, results_per_tag):
+	t = {}
+	for tag in tags:
+		search_results = GetTweets(tag, results_per_tag)
+		for tweet in search_results['statuses']:
+			#if tweet['id'] not in t:
+			t[tweet['id']] = tweet #"%s \n %s \n~@%s" % (tweet['created_at'], tweet['text'].encode('utf-8'), tweet['user']['screen_name'].encode('utf-8'))
+	return t
+
+def TweetCleaner(tweetText):
+	h = tweetText.find('http')
+	if h != -1:
+		i = tweetText[0:h-1] + " " + tweetText[h + 24:len(tweetText)]
+	else:
+		i = tweetText
+	return i
+
+def BreakToLines(text, lineLength):
+	lines = []
+	beg = 0
+	end = 0
+	while (beg + lineLength < len(text)):
+		if beg + lineLength < len(text):
+			end = text[beg:lineLength + beg].rfind(' ')
+			lines.append(text[beg:end])
+			beg = end
+		else:
+			lines.append(text[beg:len(text)])
+		print ("%s    %s" % (beg,end))
+	return lines
+
+	
+	
+tweets = TweetDict(tags, results_per_tag)
 
 for s in tweets.values():
-	print (s['text'].encode('utf-8'), '\n')
+	text = TweetCleaner(s['text'])
+	ts = time.strftime('%Y-%m-%d %H:%M:%S', time.strptime(s['created_at'],'%a %b %d %H:%M:%S +0000 %Y'))
+	lines = BreakToLines(text, 32)
+	for line in lines:
+		print (line)
+	
+	#line_1 = text[0:31]
+	#line_2 = text[32:63]
+	#line_3 = text[64:95]
+	#line_4 = text[96:125 - len(s['user']['screen_name'])] + " -@" + s['user']['screen_name']
+	#print ("%s \n %s \n %s \n %s \n" % (line_1, line_2, line_3, line_4))
